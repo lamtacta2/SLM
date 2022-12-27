13b258cf00936e09ab376ce7989d715ece25bbaf
anh231000 <72401605+anh231000@users.noreply.github.com>
2022-12-27 18:43:05 +0700

    main

--- script2.js
+++ script2.js
@@ -1,31 +1,17 @@
 var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
 
-// function unityShowBanner(type) {
-//   function updateBannerVisibility() {
-//     warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
-//   }
-//   // if (type == 'error') div.style = 'background: red; padding: 10px;';
-//   // else {
-//   //   if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-//   //   setTimeout(function() {
-//   //     updateBannerVisibility();
-//   //   }, 5000);
-//   // }
-//   updateBannerVisibility();
-// }
-
 var buildUrl = "Build";
-      var loaderUrl = buildUrl + "/WAAM.loader.js";
-      var config = {
-        dataUrl: buildUrl + "/WAAM.data.unityweb",
-        frameworkUrl: buildUrl + "/WAAM.framework.js.unityweb",
-        codeUrl: buildUrl + "/WAAM.wasm.unityweb",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "DefaultCompany",
-        productName: "WAMM_Unity",
-        productVersion: "0.1",
-      };
+var loaderUrl = buildUrl + "/SLM.loader.js";
+var config = {
+  dataUrl: buildUrl + "/SLM.data.unityweb",
+  frameworkUrl: buildUrl + "/SLM.framework.js.unityweb",
+  codeUrl: buildUrl + "/SLM.wasm.unityweb",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "DefaultCompany",
+  productName: "i4C_DigTwin",
+  productVersion: "0.1",
+};
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
 
--- script_plot_v1.js
+++ script_plot_v1.js
@@ -1,74 +1,69 @@
 var layout = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1850"};
 var layout1 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 1322"};
 var layout2 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19299"};
-var layout3 = {xaxis: {title: "time (s)"}, yaxis: {title: "Temperature (<sup>o</sup>C)"}, title: "Clab 19310"};
+
 Plotly.newPlot("myPlot",layout);
 Plotly.newPlot("myPlot1",layout1);  
 Plotly.newPlot("myPlot2",layout2);  
-Plotly.newPlot("myPlot3",layout3);  
 
 firebase
 .database()
-.ref("WAAM")
-.update({control: 0, Print: 0})
-
-firebase
-.database()
-.ref("WAAM")
+.ref("SLM")
 .on("value", function (snap) {
 
   (async() => {
 
         if (snap.val().control == 1){
 
-            let value = snap.val().value;  
-            let value1 = snap.val().value1;  
-            value = value.toFixed(1); 
-            value1 = value1.toFixed(0); 
-            console.log(value1.toString());
-            console.log(value.toString());
+            let value = snap.val().data;  
+
+            value = value.toFixed(2);
+
             const data1 = [];
             const data2 = [];
             const data3 = [];
             const data4 = [];
-            const stt = [];
+            const data5 = [];
+            const data6 = [];
   
             let k=2;
 
-            url1 = 'https://raw.githubusercontent.com/lamtacta2/WAAM/main/Data/data' + value1.toString() + value.toString() + ".csv";
+            url1 = 'https://raw.githubusercontent.com/lamtacta2/SLM/main/Data/data' + value.toString() + ".csv";
+            url2 = 'https://raw.githubusercontent.com/lamtacta2/SLM/main/Data/data1' + value.toString() + ".csv";
+            url3 = 'https://raw.githubusercontent.com/lamtacta2/SLM/main/Data/data2' + value.toString() + ".csv";
             
             let workbook1 = XLSX.read(await (await fetch(url1)).arrayBuffer());
-  
+            let workbook2 = XLSX.read(await (await fetch(url2)).arrayBuffer());
+            let workbook3 = XLSX.read(await (await fetch(url3)).arrayBuffer());
+
            function data_update(k){
             for(let i = 2; i < k; i++){
                 const locale1 = "A"+i;
                 const locale2 = "B"+i; 
-                const locale3 = "C"+i;  
-                const locale4 = "D"+i;                
-                const locale5 = "E"+i;
 
-                data1[i-2] = workbook1.Sheets.Sheet1[locale2].v;
-                data2[i-2] = workbook1.Sheets.Sheet1[locale3].v;
-                data3[i-2] = workbook1.Sheets.Sheet1[locale4].v;
-                data4[i-2] = workbook1.Sheets.Sheet1[locale5].v;
-                stt[i-2] = workbook1.Sheets.Sheet1[locale1].v;
+                data1[i-2] = workbook1.Sheets.Sheet1[locale2].v.slice(1,workbook1.Sheets.Sheet1[locale2].v.length-1);
+                data2[i-2] = workbook2.Sheets.Sheet1[locale2].v.slice(1,workbook2.Sheets.Sheet1[locale2].v.length-1);
+                data3[i-2] = workbook3.Sheets.Sheet1[locale2].v.slice(1,workbook3.Sheets.Sheet1[locale2].v.length-1);
+
+                data4[i-2] = workbook1.Sheets.Sheet1[locale1].v;
+                data5[i-2] = workbook2.Sheets.Sheet1[locale1].v;
+                data6[i-2] = workbook3.Sheets.Sheet1[locale1].v;
+           
             }}
   
             // Define Data
-            var data = [{x: stt, y: data1, mode:"lines"}];
-            var datax1 = [{x: stt, y: data2, mode:"lines"}];
-            var datax2 = [{x: stt, y: data3, mode:"lines"}];
-            var datax3 = [{x: stt, y: data4, mode:"lines"}];
+            var data = [{x: data4, y: data1, mode:"lines"}];
+            var datax1 = [{x: data5, y: data2, mode:"lines"}];
+            var datax2 = [{x: data6, y: data3, mode:"lines"}];
 
            function update(){
 
              if (k<420){
-              k = k+1;
+                k = k+1;
              if (k < 420){data_update(k);}
-             Plotly.newPlot("myPlot", data, layout);
-             Plotly.newPlot("myPlot1", datax1, layout1);
-             Plotly.newPlot("myPlot2", datax2, layout2); 
-             Plotly.newPlot("myPlot3", datax3, layout3);
+                Plotly.newPlot("myPlot", data, layout);
+                Plotly.newPlot("myPlot1", datax1, layout1);
+                Plotly.newPlot("myPlot2", datax2, layout2); 
              requestAnimationFrame(update);
 
            }}
